34095067b09eba8ba0745d2ff8c3f1257decf08d
Gabriel Kraetzer <gabriel.kraetzer@gmail.com>
2019-11-22 20:44:07 -0300

    first commit

---
+++ src/main/java/br/com/senac/marmeteria/MarmeteriaApplication.java
@@ -0,0 +1,13 @@
+package br.com.senac.marmeteria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarmeteriaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarmeteriaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/senac/marmeteria/MarmeteriaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.senac.marmeteria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarmeteriaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
